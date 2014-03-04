3e5c38d814bf9bab9ac843240931894e30cfc2ed
Guibert Timothé <timothe.guibert@outlook.fr>
2014-03-04 18:20:11 +0100

    Update rpg.c

--- PART-A/rpg.c
+++ PART-A/rpg.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "rpg.h"
-#include <time.h>
 
 int      main(void)
 {
   int   i;
-  char  **array;
-
+  char	**array;
   i = 0;
-  array = (char**)malloc(9 * sizeof(char*));
+
+  array = (char**)malloc(sizeof(char*) * 9);
   while (i < 9)
     {
       array[i] = (char*)malloc(sizeof(char) * 9);
       i = i + 1;
     }
-  
+  wallGenerator(array);
+  mapGenerator(array);
   printArray(array);
   free(array);
   return (0);
